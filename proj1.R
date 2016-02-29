2b0237b0347f6bf912e761b19ca5c6aeec3cd049
kaskarn <baldassarian@gmail.com>
2016-02-29 00:32:49 -0500

    Start fixing

--- proj1.R
+++ proj1.R
@@ -9,92 +9,68 @@ library(reshape) #to graph things!
 library(ggplot2) #to graph things nicely!
 
 #Read the data and switch from % to probability of death
-base <- read.xlsx("cardiac.xls",1,header=TRUE) 
-docs <- base[base$Procedure == "CABG",]
-docs <- docs %>% group_by(Detailed.Region) %>% mutate(r.cases = sum(Number.of.Cases), r.death = sum(Number.of.Deaths)) %>%
-        mutate(r.pdeath = r.death/r.cases) %>% mutate(r.odeath = r.pdeath/(1-r.pdeath)) %>% 
-        group_by(Hospital.Name) %>% mutate(h.cases = sum(Number.of.Cases), h.death = sum(Number.of.Deaths)) %>% 
-        mutate(h.pdeath = h.death/h.cases) %>% mutate(h.odeath = h.pdeath/(1-h.pdeath)) %>% 
-        mutate(pdeath = Number.of.Deaths/Number.of.Cases) %>% mutate(odeath = pdeath/(1-pdeath))
+docs <- read.xlsx("cardiac.xls",1,header=TRUE) %>% filter(Procedure == "CABG") %>% 
+  mutate(pdeath = Number.of.Deaths/Number.of.Cases, 
+         odeath = ifelse(Number.of.Deaths == 0, 0.000001, Number.of.Deaths / (Number.of.Cases - Number.of.Deaths))) %>%
+  mutate(edeaths = Expected.Mortality.Rate/100 * Number.of.Cases) %>%
+  group_by(Detailed.Region) %>% 
+  mutate(r.cases = sum(Number.of.Cases), r.deaths = sum(Number.of.Deaths)) %>%
+  mutate(r.pdeath = r.deaths/r.cases) %>% mutate(r.odeath = r.pdeath/(1-r.pdeath)) %>%
+  mutate(r.e_pdeath = sum(edeaths)/r.cases) %>%
+  group_by(Hospital.Name) %>% 
+  mutate(h.cases = sum(Number.of.Cases), h.deaths = sum(Number.of.Deaths)) %>% 
+  mutate(h.pdeath = h.deaths/h.cases, h.odeath = h.deaths/(h.cases - h.deaths)) %>% 
+  mutate(h.e_pdeath = sum(edeaths)/h.cases) %>% ungroup() %>%
+  mutate(Physician.Name = factor(as.character(Physician.Name), labels = unique(docs$Physician.Name))) %>%
+  mutate(Hospital.Name = factor(as.character(Hospital.Name), labels = unique(docs$Hospital.Name))) %>%
+  mutate(dnum = as.numeric(Physician.Name), hnum = as.numeric(Hospital.Name), rnum = as.numeric(Detailed.Region)) %>%
+  arrange(Physician.Name)
 
-docs$edeaths <- docs$Expected.Mortality.Rate * docs$Number.of.Cases
-docs$odeath <- ifelse(docs$odeath == 0, -10, log(docs$odeath))
-
-docs$Physician.Name <- factor(as.character(docs$Physician.Name), labels = unique(docs$Physician.Name))
-docs$Hospital.Name <- factor(as.character(docs$Hospital.Name), labels = unique(docs$Hospital.Name))
-
+D <- nrow(docs)
 R <- length(unique(docs$Detailed.Region))
 H <- length(unique(docs$Hospital.Name))
-D <- length(unique(docs$Physician.Name, Hospital.Name))
 hos.N <-  docs %>% group_by(Detailed.Region) %>% summarise(tlen = length(unique(Hospital.Name))) %$% tlen
 doc.N <- docs %>% group_by(Hospital.Name) %>% summarise(tlen = length(unique(Physician.Name))) %$% tlen
 
-#where_Hosp times a beta vector returns sum of betas by region
-where_hosp <- matrix(FALSE, nrow = R, ncol = H)
-colnames(where_hosp) <- levels(docs$Hospital.Name)
-rownames(where_hosp) <- levels(docs$Detailed.Region)
-for(i in as.numeric(unique(docs$Detailed.Region))) where_hosp[i, unique(as.numeric(docs[as.numeric(docs$Detailed.Region) == i,]$Hospital.Name))] <- TRUE
-
-where_docs <- matrix(FALSE, nrow = H, ncol = D)
-colnames(where_docs) <- unique(docs$Physician.Name)
-rownames(where_docs) <- unique(docs$Hospital.Name)
-for(i in as.numeric(unique(docs$Physician.Name))) {
-  temp <- as.numeric(unique( docs[ as.numeric(docs$Physician.Name) == i, ]$Hospital.Name )) 
-  where_docs[temp, i] <- TRUE
+I <- 10000
+#Create lists containing regional, hospital and doctor-level information
+regions <- hospitals <- doctors <- list()
+for(i in 1:R){
+  tm <- docs[docs$rnum == i,]
+  rname <- unique(tm$Detailed.Region)
+  dl <- unique(tm$hnum)
+  d <- unique(tm$r.deaths)
+  pd <- unique(tm$r.pdeath)
+  epd <- unique(tm$r.e_pdeath)
+  regions[[i]] <- list(name = rname, down = dl, deaths = d, p_death = pd, e_pdeath = epd, post_e = numeric(I))
 }
-
-#Create list containing regional, hospital and doctor-level information
-odeath <- docs[,c("odeath","Physician.Name")]  %>% distinct() %$% odeath
-odeath[odeath==0] <- 0.0001
-tm.dat <- docs[, c("Hospital.Name", "Number.of.Cases", "Number.of.Deaths")] %>% group_by(Hospital.Name) %>% mutate(all.deaths = sum(Number.of.Deaths), all.cases = sum(Number.of.Cases)) 
-tm.dat <- tm.dat[,c(1,4,5)] %>% distinct()
-reg_list <- doc_list <- hos_list <- list()
-for(i in as.numeric(unique(docs$Detailed.Region))){
-  bool <- (as.numeric(docs$Detailed.Region) == i)
-  hos <- as.numeric(unique(docs[bool,]$Hospital.Name))
-  doc <- as.numeric(unique(docs[bool,]$Physician.Name))
-  e_deathrate <-  docs %>% filter(as.numeric(Detailed.Region) == i) %>% group_by(Detailed.Region) %>% 
-                  mutate(mu = sum(edeaths/100)/sum(Number.of.Cases)) %$% unique(mu)
-  deathrate <-  docs %>% filter(as.numeric(Detailed.Region) == i)  %>% group_by(Detailed.Region) %>%
-                mutate(mu = sum(Number.of.Deaths)/sum(Number.of.Cases)) %$% unique(mu)
-  reg_list[[i]] <- list(hos = hos, doc = doc, name = as.character(factor(docs$Detailed.Region)[i]), e_mu = e_deathrate, mu = deathrate)
+for(i in 1:H){
+  tm <- docs[docs$hnum == i,]
+  rname <- unique(tm$Hospital.Name)
+  dl <- unique(tm$hnum)
+  ul <- unique(tm$rnum)
+  d <- unique(tm$r.deaths)
+  pd <- unique(tm$r.pdeath)
+  epd <- unique(tm$r.e_pdeath)
+  hospitals[[i]] <- list(name = rname, down = dl, up = ul, deaths = d, p_death = pd, e_pdeath = epd, post_e = numeric(I))
 }
-for(i in as.numeric(unique(docs$Hospital.Name))){
-  bool <- (as.numeric(docs$Hospital.Name) == i)
-  li<- as.numeric(unique(docs[bool,]$Detailed.Region))
-  do<- as.numeric(unique(docs[bool,]$Physician.Name))
-  #delta.prior <- list(df = 1, SS = diag(var(log(docs[bool,]$odeath)),sum(bool)))
-  tm <- with(tm.dat[as.numeric(tm.dat$Hospital.Name) == i,], cbind(all.cases, all.deaths))
-  e_deathrate <- docs %>% filter(as.numeric(Hospital.Name) == i) %>% group_by(Hospital.Name) %>% 
-                  mutate(mu = sum(edeaths/100)/sum(Number.of.Cases)) %$% unique(mu)
-  deathrate <-  as.numeric(docs[bool,"h.pdeath"][1,1]) 
-  r.deathrate <-  reg_list[[li]]$mu
-  hos_list[[i]] <- list(regs = li, dat = tm, docs = do, e_mu = e_deathrate, mu = deathrate, mu.r = r.deathrate)
+for(i in 1:D){
+  tm <- docs[docs$dnum == i,]
+  rname <- unique(tm$Detailed.Region)
+  ul <- unique(tm$hnum)
+  d <- unique(tm$r.deaths)
+  pd <- unique(tm$r.pdeath)
+  epd <- unique(tm$r.e_pdeath)
+  doctors[[i]] <- list(name = rname, up = ul, deaths = d, p_death = pd, e_pdeath = epd, post_e = numeric(I))
 }
-for(i in as.numeric(unique(docs$Physician.Name))){
-  bool <- (as.numeric(docs$Physician.Name) == i)
-  li <- as.numeric(unique(docs[bool,]$Hospital.Name))
-  re <- as.numeric(unique(docs[bool,]$Detailed.Region))
-  tm <- with(docs[bool,], cbind(Number.of.Cases, Number.of.Deaths))
-  e_deathrate <- docs %>% filter(as.numeric(Physician.Name) == i) %>% group_by(Physician.Name) %>% 
-    mutate(mu = sum(edeaths/100)/sum(Number.of.Cases)) %$% unique(mu)
-  deathrate <-  docs %>% filter(as.numeric(Physician.Name) == i)  %>% group_by(Physician.Name) %>%
-    mutate(mu = sum(Number.of.Deaths)/sum(Number.of.Cases)) %$% unique(mu)
-  doc_list[[i]] <- list(hos = li, dat = tm, name = as.character(factor(docs$Hospital.Name)[i]), regs = re)
-}
-
-
-#PRIORS
-#Theta, the fixed regions effect, is R-variate-normal distributed with mean mu.prior.E and variance mu.prior.V
-#Sigma, between-hospital variance is R-Inverse-Wishart distributed with df=R and simple shape matrix (for now) 
-#Gamma, between-doctor variance is H-Inverse-Wishart distributed with df=H and simple shape matrix (for now) 
+#priors
 r.odeath <- docs[,c("Detailed.Region","r.pdeath")] %>% distinct() %>% mutate(odeath = r.pdeath/(1-r.pdeath)) %$% odeath
 h.odeath <- docs[,c("Hospital.Name","h.pdeath")] %>% distinct() %>% mutate(odeath = h.pdeath/(1-h.pdeath)) %$% odeath
 d.odeath <- docs[,c("Physician.Name","Number.of.Cases","Number.of.Deaths")] %>% group_by(Physician.Name) %>% 
   mutate(pdeath = sum(Number.of.Deaths)/sum(Number.of.Cases)) %>% distinct() %>% mutate(odeath = pdeath/(1-pdeath)) %$% odeath
 
-theta.now <- theta.prior.E <- log(r.odeath)
-theta.prior.V <- diag(var(log(r.odeath)), R)
+theta_all <- theta.prior.E <- docs %>% mutate(tm = log(sum(Number.of.Deaths)/sum(Number.of.Cases))) %>% mutate(tm2 = tm/(1-tm)) %$% unique(tm2)
+theta.prior.V <- 100
 sigma.prior.df <- 1
 sigma.now <- sigma.prior.s <- diag(var(log(r.odeath)),R)
 delta.prior.df <- 1
